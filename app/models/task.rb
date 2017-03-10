b6bae6f934e1d862e54b2fb0928002966867d483
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-10 08:33:31 -0700

    finish tutorial

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,14 @@
 require 'sqlite3'
 
 class Task
-  attr_reader :task
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
     @description = task_params["description"]
     @title       = task_params["title"]
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
+    @id = task_params["id"] if task_params["id"]
   end
 
   def save
@@ -15,12 +16,21 @@ class Task
   end
 
   def self.all
-    database = SQLite3::Database.new('db/task_manager_development.db')
-    database.results_as_hash = true
     tasks = database.execute("SELECT * FROM tasks")
     tasks.map do |task|
       Task.new(task)
     end
+  end
+
+  def self.find(id)
+    task = database.execute("SELECT * FROM tasks WHERE id = ?", id).first
+    Task.new(task)
+  end
+
+  def self.database
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    database
   end
 
 end
