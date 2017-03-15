13f500a710ac49d6148034fae3e96eac997bd906
Josh Thompson <thompsonjoshd@gmail.com>
2017-03-14 23:36:03 -0600

    halfway done

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,8 +1,8 @@
-# require 'sinatra/reloader'
 require './app/models/task.rb'
 
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
+  set :method_override, true
 
   get '/' do
     erb :dashboard
@@ -24,8 +24,23 @@ class TaskManagerApp < Sinatra::Base
   end
 
   get '/tasks/:id' do
+    binding.pry
     @task = Task.find(params[:id])
     erb :show
   end
 
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
+  end
 end
